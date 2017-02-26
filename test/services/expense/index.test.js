1ee0e229e041c53129cede7262e10aee71a471e0
Daniel Chong <danielgq.chong@gmail.com>
2017-02-26 16:38:01 +1300

    added get and post test

--- test/services/expense/index.test.js
+++ test/services/expense/index.test.js
@@ -8,7 +8,7 @@ const Expense = app.service('expenses')
 const User = app.service('users')
 const authentication = require('feathers-authentication/client')
 const bodyParser = require('body-parser')
-var token, userId
+var token, userId, date
 
 // config for app to do authentication
 app
@@ -32,28 +32,28 @@ describe('expense service', () => {
         'password': 'igzSwi7*Creif4V$',
         'roles': ['admin']
       }, (res) => {
-        console.log(res)
         // setup a request to get authentication token
         chai.request(app)
-            // request to /auth/local
-            .post('/auth/local')
-            // set header
-            .set('Accept', 'application/json')
-            // send credentials
-            .send({
-              'email': 'resposadmin',
-              'password': 'igzSwi7*Creif4V$'
-            })
-            // when finished
-            .end((err, res) => {
-              // set token for auth in other requests
-              token = res.body.token
-              userId = res.body.data._id
-              done()
-            })
+        // request to /auth/local
+          .post('/auth/local')
+        // set header
+          .set('Accept', 'application/json')
+        // send credentials
+          .send({
+            'email': 'resposadmin',
+            'password': 'igzSwi7*Creif4V$'
+          })
+        // when finished
+          .end((err, res) => {
+            // set token for auth in other requests
+            token = res.body.token
+            userId = res.body.data._id
+            done()
+          })
       })
     })
   })
+
   // teardown after tests
   after((done) => {
     // delete contents of menu in mongodb
@@ -70,22 +70,76 @@ describe('expense service', () => {
     assert.ok(app.service('expenses'))
   })
 
-  it('runs create', () => {
+  it('runs create', (done) => {
+    date = (new Date()).getTime()
     app.service('expenses').create({
       owner: userId,
-      date: (new Date()).getTime(),
-      description: 'Something',
+      date: date,
+      description: 'Something2',
       amount: 99.90,
       comment: 'Some long comment'
     }).then(expense => {
       assert.ok(expense._id)
       assert.equal(expense.owner, userId)
-      assert.equal(expense.description, 'Something')
+      assert.equal(expense.date, date)
+      assert.equal(expense.description, 'Something2')
       assert.equal(expense.amount, 99.90)
       assert.equal(expense.comment, 'Some long comment')
+      done()
     })
   })
 
+  it('should post the expense data', (done) => {
+    // setup a request
+    chai.request(app)
+    // request to /store
+      .post('/expenses')
+      .set('Accept', 'application/json')
+      .set('Authorization', 'Bearer '.concat(token))
+    // attach data to request
+      .send({
+        owner: userId,
+        date: date,
+        description: 'Something',
+        amount: 99.90,
+        comment: 'Some long comment'
+      })
+    // when finished do the following
+      .end((err, res) => {
+        res.body.should.have.property('owner')
+        res.body.owner.should.equal(userId)
+        res.body.should.have.property('date')
+        res.body.date.should.equal(date)
+        res.body.should.have.property('description')
+        res.body.description.should.equal('Something')
+        res.body.should.have.property('amount')
+        res.body.amount.should.equal(99.90)
+        res.body.should.have.property('comment')
+        res.body.comment.should.equal('Some long comment')
+        done()
+      })
+  })
+
+  it('should get list of expenses', (done) => {
+    // setup a request
+    chai.request(app)
+    // request to /store
+      .get('/expenses')
+      .set('Accept', 'application/json')
+      .set('Authorization', 'Bearer '.concat(token))
+    // when finished do the following
+      .end((err, res) => {
+        res.body.should.have.property('total')
+        res.body.total.should.equal(2)
+        res.body.should.have.property('limit')
+        res.body.limit.should.equal(5)
+        res.body.should.have.property('skip')
+        res.body.skip.should.equal(0)
+        res.body.should.have.property('data')
+        res.body.data.should.have.lengthOf(2)
+        done()
+      })
+  })
   /* it('runs create', () => {
    *   app.service('expenses').patch({
    *     date: (new Date()).getTime(),
