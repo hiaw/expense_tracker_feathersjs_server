63637130da82f62d6355c785d1a821627078fd9b
Daniel Chong <danielgq.chong@gmail.com>
2017-02-26 20:19:34 +1300

    able to CRUD expense as admin

--- test/services/expense/rest.test.js
+++ test/services/expense/rest.test.js
@@ -8,7 +8,7 @@ const Expense = app.service('expenses')
 const User = app.service('users')
 const authentication = require('feathers-authentication/client')
 const bodyParser = require('body-parser')
-var token, userId, date
+var token, userId, date, expenseId
 
 // config for app to do authentication
 app
@@ -67,7 +67,7 @@ describe('REST expense service', () => {
     })
   })
 
-  it('should post the expense data', (done) => {
+  it('should create the expense data', (done) => {
     // setup a request
     chai.request(app)
     // request to /store
@@ -84,7 +84,8 @@ describe('REST expense service', () => {
       })
     // when finished do the following
       .end((err, res) => {
-        console.log(res.body)
+        expenseId = res.body._id
+
         res.body.should.have.property('owner')
         res.body.owner.should.equal(userId)
 
@@ -103,6 +104,28 @@ describe('REST expense service', () => {
       })
   })
 
+  it('should create another the expense data', (done) => {
+    // setup a request
+    chai.request(app)
+    // request to /store
+      .post('/expenses')
+      .set('Accept', 'application/json')
+      .set('Authorization', 'Bearer '.concat(token))
+    // attach data to request
+      .send({
+        owner: userId,
+        date: date,
+        description: 'Something',
+        amount: 99.90,
+        comment: 'Some long comment'
+      })
+    // when finished do the following
+      .end((err, res) => {
+        res.body._id.should.not.equal(expenseId)
+        done()
+      })
+  })
+
   it('should get list of expenses', (done) => {
     // setup a request
     chai.request(app)
@@ -113,13 +136,81 @@ describe('REST expense service', () => {
     // when finished do the following
       .end((err, res) => {
         res.body.should.have.property('total')
-        res.body.total.should.equal(1)
+        res.body.total.should.equal(2)
 
         res.body.should.have.property('limit')
         res.body.limit.should.equal(5)
 
         res.body.should.have.property('skip')
         res.body.skip.should.equal(0)
+
+        res.body.should.have.property('data')
+        res.body.data.should.have.lengthOf(2)
+        done()
+      })
+  })
+
+  it('should get the expense', (done) => {
+    // setup a request
+    chai.request(app)
+    // request to /store
+      .get('/expenses/' + expenseId)
+      .set('Accept', 'application/json')
+      .set('Authorization', 'Bearer '.concat(token))
+    // when finished do the following
+      .end((err, res) => {
+        res.body._id.should.equal(expenseId)
+        done()
+      })
+  })
+
+  it('should update the expense', (done) => {
+    // setup a request
+    chai.request(app)
+    // request to /store
+      .patch('/expenses/' + expenseId)
+      .set('Accept', 'application/json')
+      .set('Authorization', 'Bearer '.concat(token))
+    // attach data to request
+      .send({
+        amount: 9.90
+      })
+    // when finished do the following
+      .end((err, res) => {
+        res.body.should.have.property('description')
+        res.body.description.should.equal('Something')
+
+        res.body.should.have.property('amount')
+        res.body.amount.should.equal(9.90)
+        done()
+      })
+  })
+
+  it('should delete the expense', (done) => {
+    // setup a request
+    chai.request(app)
+    // request to /store
+      .delete('/expenses/' + expenseId)
+      .set('Accept', 'application/json')
+      .set('Authorization', 'Bearer '.concat(token))
+    // when finished do the following
+      .end((err, res) => {
+        res.statusCode.should.equal(200)
+        done()
+      })
+  })
+
+  it('should now only give single expense', (done) => {
+    // setup a request
+    chai.request(app)
+    // request to /store
+      .get('/expenses')
+      .set('Accept', 'application/json')
+      .set('Authorization', 'Bearer '.concat(token))
+    // when finished do the following
+      .end((err, res) => {
+        res.body.should.have.property('total')
+        res.body.total.should.equal(1)
 
         res.body.should.have.property('data')
         res.body.data.should.have.lengthOf(1)
